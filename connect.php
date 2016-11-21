f30079f8a1a2e9a7d1de10c50319401bdd1af6c5
dinhduy7895 <dinhvanduy7895@gmail.com>
2016-11-21 11:37:44 +0700

    update

--- connect.php
+++ connect.php
@@ -1,4 +1,18 @@
-<?php 
-	$conn = mysqli_connect('localhost','root','binh','StudyAbroad');
-	$conn->mysql_set_charset('utf8');
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "studyabroad";
+try{
+     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e)
+    {
+    echo  $e->getMessage();
+    }
+include('class.user.php');
+$user = new USER($conn);
 ?>
+
